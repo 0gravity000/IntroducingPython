a32d8ee184ad1294275b088300547efb032ed1db
0gravity000 <0gravity000@gmail.com>
2021-09-07 20:27:11 +0900

    Add 020311.py

---
+++ 020311.py
@@ -0,0 +1,22 @@
+# 多彩な文字列操作
+poem = '''All tjat doth flow we cannot liquid name
+Or else would fire and water be the same;
+But that is liquid whice is moist and wet
+Fire that property can never get.
+Then 'tis not cold that doth the fire put out
+But 'tis the wet that makes it die, no doubt.'''
+
+# 0-12を取り出し
+poem[:13]
+
+len(poem)
+
+poem.startswith('All')
+
+poem.endswith('That\'s all, folks!')
+
+word = 'the'
+poem.find(word)
+poem.rfind(word)
+poem.count(word)
+poem.isalnum()
