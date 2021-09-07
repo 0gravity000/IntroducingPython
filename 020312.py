cbac2e9fe711993d350b396bfb1cf1b4103b3503
0gravity000 <0gravity000@gmail.com>
2021-09-07 20:35:39 +0900

    Add 020312.py

---
+++ 020312.py
@@ -0,0 +1,13 @@
+# 大文字と小文字の区別、配置
+setup = 'a duck goes into a bar...'
+print(setup)
+
+setup.strip('.')
+setup.capitalize()
+setup.title()
+setup.upper()
+setup.lower()
+setup.swapcase()
+setup.center(30)
+setup.ljust(30)
+setup.rjust(30)
