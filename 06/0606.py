e7c66d25674eb32adfc3f6f40ef45df647d6b954
0gravity000 <0gravity000@gmail.com>
2021-09-08 22:41:33 +0900

    0606を編集

--- 06/0606.py
+++ 06/0606.py
@@ -1,14 +1,28 @@
 # 6.6 superによる親への支援要請
+
+# 親クラス
 class Person():
     def __init__(self, name):
         self.name = name
 
+# 子クラス
+# 子クラスが親メソッドを呼び出したい時は、super()を使う
+# __init__()をオーバーライドしている
 class EmailPerson(Person):
     def __init__(self, name, email):
         super().__init__(name)
         self.email = email
 
+# オブジェクト(インスタンス)を作る
 bob = EmailPerson('Bob Frapples', 'bob@frapples.com')
 
+# プロパティにアクセスする
 bob.name
 bob.email
+
+# 以下のようにも書けるけど、継承の意味がなくなってしまう
+# super().__init__(name) することで、親クラスが変更しても反映される
+class EmailPerson(Person):
+    def __init__(self, name, email):
+        self.__init__(name) #ココに注意
+        self.email = email
