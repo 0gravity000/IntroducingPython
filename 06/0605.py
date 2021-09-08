fa2d6c6de38f3f6e62ac994ddf4250f0d9ad8042
0gravity000 <0gravity000@gmail.com>
2021-09-08 21:40:09 +0900

    0605を編集

--- 06/0605.py
+++ 06/0605.py
@@ -1,8 +1,12 @@
 # 6.5 メソッドの追加
+
+# 親クラス
 class Car1():
     def exclaim(self):
         print("I'm a Car!")
 
+# 子クラス
+# 子クラスは親クラスにないメソッドneed_a_push()を追加できる
 class Yugo1(Car1):
     def exclaim(self):
         print("I'm a Yugo! Much like a Car, but more Yugo-ish.")
@@ -10,10 +14,12 @@ class Yugo1(Car1):
     def need_a_push(self):
         print("A little help here?")
 
+# オブジェクト(インスタンス)を作る
 give_me_a_car = Car1()
 give_me_a_yugo = Yugo1()
 
+# 子クラスの追加メソッドneed_a_push()を呼び出し
 give_me_a_yugo.need_a_push()
 
-# エラー
+# 親クラスでは、追加メソッドneed_a_push()を呼び出しエラー
 give_me_a_car.need_a_push()
