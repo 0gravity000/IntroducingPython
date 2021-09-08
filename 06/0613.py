f5f294154394cc5307a203274f621983c8bdba8d
0gravity000 <0gravity000@gmail.com>
2021-09-09 01:20:04 +0900

    0613を編集

--- 06/0613.py
+++ 06/0613.py
@@ -1,12 +1,19 @@
 # 6.13 コンポジション
+# 継承はis-aの関係（子クラスは親クラスである）
+# コンポジションはhas-aの関係（xクラスはyクラスを持っている）
+# アヒルはくちばし(bill)、しっぽ(tail)をもっている
+
+# くちばしクラス
 class Bill():
     def __init__(self, description):
         self.description = description
 
+# しっぽクラス
 class Tail():
     def __init__(self, length):
         self.length = length
 
+# アヒルクラス
 class Duck():
     def __init__(self, bill, tail):
         self.bill = bill
@@ -14,7 +21,8 @@ class Duck():
     def about(self):
         print('This duck has a', self.bill.description, 'bill and a', self.tail.length, 'tail')
 
+# オブジェクト(インスタンス)を作る
 tail = Tail('long')
 bill = Bill('wide orange')
-duck = Duck(bill, tail)
+duck = Duck(bill, tail) #アヒルはくちばし(bill)、しっぽ(tail)をもっている
 duck.about()
