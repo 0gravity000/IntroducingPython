961823de00dfb01b9ab407cf73a046df7e417631
0gravity000 <0gravity000@gmail.com>
2021-09-09 00:18:59 +0900

    0610を編集

--- 06/0610.py
+++ 06/0610.py
@@ -1,23 +1,33 @@
 # 6.10 メソッドのタイプ
+# メソッドの第1引数がselfなら、インスタンスメソッド（いわゆる普通のメソッド）
+# @acakkmethodデコレータと第1引数がclsなら、クラスメソッド
 class A():
     count = 0
     def __init__(self):
-        A.count += 1    # self.countだとインスタンスの属性になる
+        A.count += 1    # self.countだとインスタンスの属性になってしまう
     def exclaim(self):
         print("I'm an A!")
     @classmethod
     def kids(cls):  # クラスメソッド
-        print("A has", cls.count, "Little objects")
+        print("A has", cls.count, "Little objects") # A.countでもOK
 
+# オブジェクト(インスタンス)を作る
 easy_a = A()
 breezy_a = A()
 wheezy_a = A()
+# クラスメソッドを呼び出す
 A.kids()
 
 # 静的メソッド
+# クラスにもオブジェクト(インスタンス)にも影響を与えない
+# 独立した存在としてふらふらしているよりも都合がいいので、
+# クラス定義の中にいるだけのもの
+# @staticmethodデコレータを付けると、静的メソッド
+# 第1引数にselfやclsを取らない
 class CoyoteWeapon():
     @staticmethod
-    def commercial():
+    def commercial():   #静的メソッド
         print('This CoyoteWeapon has brought to you by Acme')
 
+# オブジェクト(インスタンス)を作らず、実行できる
 CoyoteWeapon.commercial()
