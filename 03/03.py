60fb368351154d54e93585edcb500e531d59ec54
0gravity000 <0gravity000@gmail.com>
2021-09-08 09:15:50 +0900

    03.pyを編集

--- 03/03.py
+++ 03/03.py
@@ -1,11 +1,15 @@
+# 3.2.1 []またはlist()による作成
+# リストは、0個以上の要素をカンマで区切り、[]で囲む
 empty_list = []
 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 big_birds = ['emu', 'ostrich', 'cassowary']
 first_names = ['Graham', 'John', 'Terry', 'Terry', 'Michael']
 
+# list()関数で空リストを作ることもできる
 another_empty_list = list()
 another_empty_list
 
+# 3.2.2 list()によるほかのデータ型からリストへの変換
 list('cat')
 
 a_tuple = ('ready', 'fire', 'aim')
@@ -19,20 +23,43 @@ splitme.split('/')
 
 splitme.split('//')
 
+# 3.2.3 [offset]を使った要素の取り出し
 marxes = ['Groucho', 'Chico', 'Harpo']
 marxes[0]
+marxes[1]
+marxes[2]
+marxes[-1]
+marxes[-2]
+marxes[-3]
 
+# 3.2.4 リストのリスト
 small_birds = ['hummingbird', 'finch']
 extinct_birds = ['dodo', 'passenger pigeon', 'Norwegian Blue']
 carol_birds = [3, 'French hens', 2, 'turtledoves']
 all_birds = [small_birds, extinct_birds, 'macaw', carol_birds]
+all_birds
+all_birds[0]
+all_birds[1]
+all_birds[1][0]
 
+# 3.2.5 [offset]による要素の書き換え
+marxes = ['Groucho', 'Chico', 'Harpo']
+marxes[2] = 'Wanda'
+marxes
+
+# 3.2.6 オフセットの範囲を指定したスライスによるサブシーケンスの取り出し
 marxes = ['Groucho', 'Chico', 'Harpo']
 marxes[0:2]
+marxes[::2]
+marxes[::-2]
+marxes[::-1]
 
+# 3.2.7 append()による末尾への要素の追加
+marxes = ['Groucho', 'Chico', 'Harpo']
 marxes.append('Zeppo')
 marxes
 
+# 3.2.8 extend()または+=を使ったリストの結合
 marxes = ['Groucho', 'Chico', 'Harpo', 'Zeppo']
 others = ['Gummo', 'Karl']
 marxes.extend(others)
@@ -48,6 +75,7 @@ others = ['Gummo', 'Karl']
 marxes.append(others)
 marxes
 
+# 3.2.9 insert()によるオフセットを指定した要素の追加
 marxes = ['Groucho', 'Chico', 'Harpo', 'Zeppo']
 marxes.insert(3, 'Gummo')
 marxes
@@ -55,6 +83,7 @@ marxes
 marxes.insert(10, 'Karl')
 marxes
 
+# 3.2.10 delによる指定したオフセットの要素の削除
 del marxes[-1]
 marxes
 
@@ -64,19 +93,23 @@ del marxes[2]
 marxes
 marxes[2]
 
+# 3.2.11 remove()による値に基づく要素の削除
 marxes = ['Groucho', 'Chico', 'Harpo', 'Gummo', 'Zeppo']
 marxes.remove('Gummo')
 marxes
 
+# 3.2.12 pop()でオフセットを指定して用をを取り出し、削除する方法
 marxes = ['Groucho', 'Chico', 'Harpo', 'Gummo', 'Zeppo']
 marxes.pop()
 marxes
 marxes.pop(1)
 marxes
 
+# 3.2.13 index()により要素の値から要素のオフセットを知る方法
 marxes = ['Groucho', 'Chico', 'Harpo', 'Zeppo']
 marxes.index('Chico')
 
+# 3.2.14 inを使った値の有無のテスト
 marxes = ['Groucho', 'Chico', 'Harpo', 'Zeppo']
 'Groucho' in marxes
 'Bob' in marxes
@@ -84,6 +117,7 @@ marxes = ['Groucho', 'Chico', 'Harpo', 'Zeppo']
 words = ['a', 'deer', 'a', 'female', 'deer']
 'deer' in words
 
+# 3.2.15 count()を使った値の個数の計算
 marxes = ['Groucho', 'Chico', 'Harpo']
 marxes.count('Harpo')
 
@@ -92,8 +126,9 @@ marxes.count('Bob')
 snl_skit = ['cheesebuger', 'cheesebuger', 'cheesebuger']
 snl_skit.count('cheesebuger')
 
+# 3.2.16 join()による文字列への変換
 marxes = ['Groucho', 'Chico', 'Harpo']
-', ' .join(marxes)  #join()はspｨt()の逆
+', ' .join(marxes)  #join()はsplit()の逆
 
 friends = ['Harry', 'Hermione', 'Ron']
 separator = ' * '
@@ -103,6 +138,7 @@ separated = joined.split(separator)
 separated
 separated == friends
 
+# 3.2.17 sort()による要素の並べ替え
 marxes = ['Groucho', 'Chico', 'Harpo']
 sorted_marxes = sorted(marxes)
 sorted_marxes
@@ -118,9 +154,11 @@ numbers = [2, 1, 4.0, 3]
 numbers.sort(reverse=True)
 numbers
 
+# 3.2.18 len()による長さの取得
 marxes = ['Groucho', 'Chico', 'Harpo']
 len(marxes)
 
+# 3.2.19 =による代入とcopy()によるコピー
 a = [1, 2, 3]
 a
 b = a
@@ -143,15 +181,20 @@ b
 c
 d
 
+# 3.3 タプル
+# 3.3.1 ()を使ったタプルの作成
 empty_tuple = ()
 empty_tuple
 
+# 末尾にカンマを付けてタプルを作成
 one_marx = 'Groucho',
 one_marx
 
+# 要素が複数の場合、カンマで区切る。最後の要素の後ろのカンマは省略できる
 marx_tuple = 'Groucho', 'Chico', 'Harpo'
 marx_tuple
 
+# 値全体を()で囲むとタプルと分かりやすい
 marx_tuple = ('Groucho', 'Chico', 'Harpo')
 marx_tuple
 
@@ -167,13 +210,20 @@ password, icecream = icecream, password
 password
 icecream
 
+# 変換関数のtuple()を使う
 marx_list = ['Groucho', 'Chico', 'Harpo']
 tuple(marx_list)
 
-#辞書 連想配列 ハッシュ ハッシュマップ dict
+# 3.4 辞書
+# 辞書 連想配列 ハッシュ ハッシュマップ dictとも呼ばれる
+# 3.4.1 {}による作成
 empty_dict = {}
 empty_dict
 
+# 3.4.2 dict()を使った変換
+# { key:value }
+# 2つの値のシーケンスを辞書に変換する
+# 2要素のシーケンスを含むものなら何でも使える
 lol = [['a', 'b'], ['c', 'b'], ['e', 'f']]
 dict(lol)
 
@@ -189,6 +239,7 @@ dict(los)
 tos = ('ab', 'cd', 'ef')
 dict(tos)
 
+# 3.4.3 [key]による要素の追加、変更
 pythons = {
     'Chapman': 'Graham',
     'Cleese': 'John',
@@ -198,6 +249,7 @@ pythons = {
 }
 pythons
 
+# 辞書のキーは一意でなくてはならない
 pythons['Gilliam'] = 'Garry'
 pythons
 
@@ -214,6 +266,7 @@ some_pythons = {
 }
 some_pythons
 
+# 3.4.4 update()による辞書の結合
 pythons = {
     'Chapman': 'Graham',
     'Cleese': 'John',
@@ -231,16 +284,28 @@ second = {'b': 'platypus'}
 first.update(second)
 first
 
+# 3.4.5 delによる指定したキーを持つ要素の削除
+pythons = {
+    'Chapman': 'Graham',
+    'Cleese': 'John',
+    'Idle': 'Eric',
+    'Jones': 'Terry',
+    'Falin': 'Michael',
+    'Marx': 'Groucho',
+    'Howard': 'Moe',
+}
 del pythons['Marx']
 pythons
 del pythons['Howard']
 pythons
 
+# 3.4.6 clear()によるすべての要素の削除
 pythons.clear()
 pythons
 pythons = {}
 pythons
 
+# 3.4.7 inを使ったキーの有無のテスト
 pythons = {
     'Chapman': 'Graham',
     'Cleese': 'John',
@@ -252,19 +317,36 @@ pythons
 'Falin' in pythons
 'Gilliam' in pythons
 
+# 3.4.8 [key]による要素の取得
+pythons = {
+    'Chapman': 'Graham',
+    'Cleese': 'John',
+    'Jones': 'Terry',
+    'Falin': 'Michael',
+}
 pythons['Cleese']
 pythons['Marx']
 'Marx' in pythons
+# 辞書専用のget()関数 キーがあればその値が返される
+pythons.get('Cleese')
+
+# キーなければ指定したオプション値が返される
 pythons.get('Marx', 'Not a Python')
+# オプション値を指定しなかれなNoneになる。インタープリタには何も表示されない
 pythons.get('Marx')
 
+# 3.4.9 keys()によるすべてのキーの取得
 signals = {'green': 'go', 'yellow': 'go faster', 'red': 'smile for the camera'}
 signals.keys()
+list(signals.keys())
 
+# .4.10 values()によるすべての値の取得
 list(signals.values())
 
+# .4.11 items()によるすべてのキー/値ペアの取得
 list(signals.items())
 
+# =による代入とcopy()によるコピー
 signals = {'green': 'go', 'yellow': 'go faster', 'red': 'smile for the camera'}
 save_signals = signals
 signals['blue'] = 'confuse everyone'
@@ -276,21 +358,28 @@ signals['blue'] = 'confuse everyone'
 signals
 original_signals
 
+# 3.5 集合
+# 3.5.1 set()による作成
 empty_set = set()
 empty_set
+# 1個以上のカンマ区切りの値を{}で囲む
 even_numbers = {0, 2, 4, 6, 8}
 even_numbers
 odd_numbers = {1, 3, 5, 7, 9}
 odd_numbers
 
+# 3.5.2 set()によるほかのデータ型から集合への変換
+# 重複する'e'と't'は1つになる
 set('letters')
 
 set(['Dasher', 'Dancer', 'Prancer', 'Mason-Dixon'])
 
 set(('Ummagumma', 'Echoes', 'Atom Heart Mother'))
 
+# 辞書を渡すと、キーだけが使われる
 set({'apple': 'red', 'orage': 'orange', 'cherry': 'red'})
 
+# 3.5.3 inを使った値の有無のテスト
 drinks = {
     'martini': {'vodka', 'vermouth'},
     'black russian': {'vodka', 'kahlua'},
@@ -307,6 +396,7 @@ for name, contents in drinks.items():
     if 'vodka' in contents and not ('vermouth' in contents or 'cream' in contents):
         print(name)
 
+# 3.5.4 組み合わせと演算
 for name, contents in drinks.items():
     if contents & {'vermouth', 'orange juice'}:
         print(name)
@@ -315,6 +405,7 @@ for name, contents in drinks.items():
     if 'vodka' in contents and not contents & {'vermouth', 'cream'}:
         print(name)
 
+# 3.6 データ構造の比較
 marx_list = ['Groucho', 'Chico', 'Harpo']
 marx_tuple = 'Groucho', 'Chico', 'Harpo'
 marx_dict = {'Groucho': 'banjo', 'Chico': 'piano', 'Harpo': 'harp'}
@@ -322,6 +413,7 @@ marx_list[2]
 marx_tuple[2]
 marx_dict['Harpo']
 
+# 3.7 もっと大きいデータ構造
 marxes = ['Groucho', 'Chico', 'Harpo']
 pythons = ['Chapman', 'Cleese', 'Gilliam', 'Jones', 'Palin']
 stooges = ['Moe', 'Curly', 'Larry']
@@ -335,6 +427,8 @@ list_of_lists
 dict_of_lists = {'Marxes': marxes, 'Pythons': pythons, 'Stooges':stooges}
 dict_of_lists
 
+# タプルは辞書のキーになれる（イミュータブルのため）
+# リスト、辞書、集合は辞書のキーになれない
 houses = {
     (44.79, -93.14, 285): 'My House',
     (38.89, -77.03, 13): 'The White House',
