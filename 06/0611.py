b995675e41fdb40da16fa21e7f79d08dab657c00
0gravity000 <0gravity000@gmail.com>
2021-09-09 00:36:55 +0900

    0611を編集

--- 06/0611.py
+++ 06/0611.py
@@ -1,4 +1,6 @@
 # 6.11 ダックタイピング
+
+# 親クラス
 class Quote():
     def __init__(self, person, words):
         self.person = person
@@ -8,12 +10,14 @@ class Quote():
     def says(self):
         return self.words + '.'
 
+# 子クラス
 class QuestionQuote(Quote):
-    def says(self):
+    def says(self): #says()メソッドをオーバーライド
         return self.words + '?'
 
+# 子クラス
 class ExclamationQuote(Quote):
-    def says(self):
+    def says(self): #says()メソッドをオーバーライド
         return self.words + '!'
 
 hunter = Quote('Elmer Fudd', "I'm hunting wabbits")
@@ -24,6 +28,12 @@ print(hunted1.who(), 'says:', hunted1.says())
 
 hunted2 = ExclamationQuote('Daffy Duck', "It's rabbit season")
 print(hunted2.who(), 'says:', hunted2.says())
+
+# 異なる3種類のsays()メソッドが3つのクラスのために異なる動作をする
+# これがオブジェクト指向言語の店頭的なポリモーフィズム
+# who()、says()メソッドを持ちさえすれば、どのようなオブジェクトであっても
+# 継承などを利用しなくても、
+# 共通のインターフェイスを持つオブジェクトとして扱うことができる
 
 class BabblingBrook():
     def who(self):
@@ -39,4 +49,8 @@ def who_says(obj):
 who_says(hunter)
 who_says(hunted1)
 who_says(hunted2)
+# これは、他とまったく無関係
 who_says(brook)
+
+# このような動作は古いことわざにちなんでダックタイピングと呼ばれる
+# アヒルのように歩き、アヒルのようにクワッと鳴くなら、それはアヒルだ
