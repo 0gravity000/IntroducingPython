ef5c2c919d98b41db6758c474af67e211ba21833
0gravity000 <0gravity000@gmail.com>
2021-09-26 17:40:23 +0900

    0401、0402を追加

---
+++ 04/0402.py
@@ -0,0 +1,18 @@
+# 4.2 \による行の継続
+alphabet = ''
+alphabet += 'abcdefg'
+alphabet += 'hijklmnop'
+alphabet += 'qrstuv'
+alphabet += 'wxyz'
+
+# 継続文字\を使う 
+alphabet = 'abcdefg'+ \
+  'hijklmnop'+ \
+  'qrstuv'+ \
+  'wxyz'
+
+''' 
+コマンドラインで継続文字\を使う
+>>> 1 + 2 + \
+  3
+'''
