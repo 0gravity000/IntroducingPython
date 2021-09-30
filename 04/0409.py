5d3281dbe37ed1a08d71cb6a36841781f9ac0ccf
0gravity000 <0gravit000@gmail.com>
2021-09-30 10:51:44 +0900

    0409を編集

--- 04/0409.py
+++ 04/0409.py
@@ -1,4 +1,7 @@
 # 4.9 デコレータ
+# デコレータは入力として関数をひとつ取り、別の関数を返す関数
+
+# デコレータを定義
 def document_it(func):
     def new_function(*args, **kwargs):
         print('Running function:', func.__name__)
@@ -18,25 +21,42 @@ add_ints(3, 5)
 cooler_add_ints = document_it(add_ints) #手作業でデコレータの戻り値を代入
 cooler_add_ints(3, 5)
 
+# デコレートしたい関数の直前に、@decorator_nameを追加する
 @document_it
 def add_ints(a, b):
     return a + b
 
 add_ints(3, 5)
+'''
+Running function: add_ints
+Positional arguments: (3, 5)
+Keyword arguments: {}
+Result: 8
+8
+'''
 
-#
+
+# 別のデコレータを定義
 def square_it(func):
     def new_function(*args, **kwargs):
         result = func(*args, **kwargs)
         return result * result
     return new_function
 
+# 複数のデコレータを実行
 @document_it
 @square_it
 def add_ints(a, b):
     return a + b
 
 add_ints(3, 5)
+'''
+Running function: new_function
+Positional arguments: (3, 5)
+Keyword arguments: {}
+Result: 64
+64
+'''
 
 #
 @square_it
@@ -45,3 +65,10 @@ def add_ints(a, b):
     return a + b
 
 add_ints(3, 5)
+'''
+Running function: add_ints
+Positional arguments: (3, 5)
+Keyword arguments: {}
+Result: 8
+64
+'''
